87e88d43c6cbed99bebd669c7a27c0ad78e0c56b
Sebastian Mueller <sebastian@sourceboat.com>
2017-08-05 14:35:14 +0200

    Add try catch block around everything

--- my-first-alexa-skill.js
+++ my-first-alexa-skill.js
@@ -1,33 +1,38 @@
 exports.handler = (event, context, callback) => {
-    if (event.session.new) {
-      // New Session
-      console.log('New Session')
-    }
+    try {
+        if (event.session.new) {
+          // New Session
+          console.log('New Session')
+        }
 
-    switch (event.request.type) {
-        case 'LaunchRequest':
-            console.log('LaunchRequest')
-            break;
+        switch (event.request.type) {
+            case 'LaunchRequest':
+                console.log('LaunchRequest')
+                break;
 
-        case 'IntentRequest':
-            context.succeed({
-              version: "1.0",
-              sessionAttributes: {},
-              response: {
-                outputSpeech: {
-                  type: 'PlainText',
-                  text: 'Hello from my first alexa skill'
-                },
-                shouldEndSession: true
-              }
-            })
-            break;
+            case 'IntentRequest':
+                context.succeed({
+                  version: "1.0",
+                  sessionAttributes: {},
+                  response: {
+                    outputSpeech: {
+                      type: 'PlainText',
+                      text: 'Hello from my first alexa skill'
+                    },
+                    shouldEndSession: true
+                  }
+                })
+                break;
 
-        case 'SessionEndedRequest':
-            console.log('LaunchRequest')
-            break;
+            case 'SessionEndedRequest':
+                console.log('LaunchRequest')
+                break;
 
-        default:
-            context.fail(`INVALID REQUEST TYPE ${event.target.type}`)
+            default:
+                context.fail(`INVALID REQUEST TYPE ${event.target.type}`)
+        }
+    } catch (error) {
+        console.log('Got an error: ', error)
+        context.fail(`got an error: ${error}`)
     }
 }
