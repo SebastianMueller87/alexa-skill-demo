87c7fc6133c4ae788937d3749e9651875ba628f1
Sebastian Müller <waschtiaukrug@googlemail.com>
2017-08-05 17:33:35 +0200

    Update my-first-alexa-skill.js

--- my-first-alexa-skill.js
+++ my-first-alexa-skill.js
@@ -47,7 +47,7 @@ exports.handler = (event, context, callback) => {
                 break;
 
             case 'SessionEndedRequest':
-                console.log('LaunchRequest')
+                console.log('SessionEndedRequest')
                 break;
 
             default:
